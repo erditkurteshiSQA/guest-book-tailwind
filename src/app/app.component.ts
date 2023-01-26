075029247514d4379bd5a4eba34de543e1298934
Erdit Kurteshi <erdit.kurteshi@sqa-consulting.com>
2023-01-26 14:26:02 +0100

    added wallet-selector and tailwind css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,110 @@
-import { Component } from '@angular/core';
+import type { OnInit } from "@angular/core";
+import { Component } from "@angular/core";
+import type { WalletSelector, AccountState } from "@near-wallet-selector/core";
+import { setupWalletSelector } from "@near-wallet-selector/core";
+// import { setupDefaultWallets } from "@near-wallet-selector/default-wallets";
+import { setupSender } from "@near-wallet-selector/sender";
+// import { setupXDEFI } from "@near-wallet-selector/xdefi";
+// import { setupNearFi } from "@near-wallet-selector/nearfi";
+// import { setupMathWallet } from "@near-wallet-selector/math-wallet";
+// import { setupNightly } from "@near-wallet-selector/nightly";
+// import { setupMeteorWallet } from "@near-wallet-selector/meteor-wallet";
+// import { setupWelldoneWallet } from "@near-wallet-selector/welldone-wallet";
+// import { setupNightlyConnect } from "@near-wallet-selector/nightly-connect";
+// import { setupWalletConnect } from "@near-wallet-selector/wallet-connect";
+// import { setupCoin98Wallet } from "@near-wallet-selector/coin98-wallet";
+// import { setupHereWallet } from "@near-wallet-selector/here-wallet";
+// import { setupNeth } from "@near-wallet-selector/neth";
+// import { setupOptoWallet } from "@near-wallet-selector/opto-wallet";
+import { setupLedger } from "@near-wallet-selector/ledger";
+import { setupNearWallet } from "@near-wallet-selector/near-wallet";
+import { setupModal } from "@near-wallet-selector/modal-ui";
+import type { WalletSelectorModal } from "@near-wallet-selector/modal-ui";
+// import { setupModal } from "@near-wallet-selector/modal-ui-js";
+// import type { WalletSelectorModal } from "@near-wallet-selector/modal-ui-js";
+
+declare global {
+  interface Window {
+    selector: WalletSelector;
+    modal: WalletSelectorModal;
+  }
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  title = 'nearAngularWithTailwind';
+export class AppComponent implements OnInit {
+  selector: WalletSelector;
+  modal: WalletSelectorModal;
+  accountId: string | null;
+  accounts: Array<AccountState> = [];
+
+  async ngOnInit() {
+    await this.initialize().catch((err) => {
+      console.error(err);
+      alert("Failed to initialise wallet selector");
+    });
+  }
+
+  async initialize() {
+    const _selector = await setupWalletSelector({
+      network: "testnet",
+      debug: true,
+      modules: [
+        // ...(await setupDefaultWallets()),
+        setupNearWallet(),
+        setupSender(),
+        // setupXDEFI(),
+        // setupMathWallet(),
+        // setupNightly(),
+        // setupMeteorWallet(),
+        // setupWelldoneWallet(),
+        // setupHereWallet(),
+        // setupCoin98Wallet(),
+        setupLedger()
+        // setupNearFi(),
+        // setupNeth({
+        //   bundle: false,
+        // }),
+        // setupOptoWallet(),
+        // setupWalletConnect({
+        //   projectId: "c4f79cc...",
+        //   metadata: {
+        //     name: "NEAR Wallet Selector",
+        //     description: "Example dApp used by NEAR Wallet Selector",
+        //     url: "https://github.com/near/wallet-selector",
+        //     icons: ["https://avatars.githubusercontent.com/u/37784886"],
+        //   },
+        // }),
+        // setupNightlyConnect({
+        //   url: "wss://relay.nightly.app/app",
+        //   appMetadata: {
+        //     additionalInfo: "",
+        //     application: "NEAR Wallet Selector",
+        //     description: "Example dApp used by NEAR Wallet Selector",
+        //     icon: "https://near.org/wp-content/uploads/2020/09/cropped-favicon-192x192.png",
+        //   },
+        // }),
+      ],
+    });
+
+    const _modal = setupModal(_selector, { contractId: 'guest-book.testnet' });
+    const state = _selector.store.getState();
+
+    this.accounts = state.accounts;
+    this.accountId =
+      state.accounts.find((account) => account.active)?.accountId || null;
+
+    window.selector = _selector;
+    window.modal = _modal;
+
+    this.selector = _selector;
+    this.modal = _modal;
+  }
+
+  openModal() {
+    this.modal.show();
+  }
 }
